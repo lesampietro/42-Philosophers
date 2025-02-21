fd2443d9fc13851af9003a5a3b76c2f6b4ec263d
Letícia Sampietro <lsampiet@student.42sp.org.br>
2025-02-21 16:23:25 -0300

    refactor error messges and function

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -5,13 +5,13 @@ void	error_n_exit(char *msg, int msg_type)
 	if (msg == NULL && msg_type == 1)
 	{
 		printf(RED"Error: Wrong number of arguments\n"RST);
-		printf(YEL"Usage example: ./philo <nbr_philosophers> <time_to_die> ");
+		printf(CYN"Usage example: ./philo <nbr_philosophers> <time_to_die> ");
 		printf("<time_to_eat> <time_to_sleep> <nbr_of_meals>\n"RST);
 	}
 	else if (!msg_type)
 		printf(RED"%s"RST, msg);
 	else
-		printf(RED "Unknown Error\n" RST);
+		printf(RED"Unknown Error\n"RST);
 	exit(1);
 }
 
